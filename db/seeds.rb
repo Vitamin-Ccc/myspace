fab560bbd921275de3d929ebcddbb92be6e3471d
Sky Lin <linsky519@gmail.com>
2022-01-02 23:33:52 -0700

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(email: "hello@gmail.com", password: 857392)
+u2 = User.create(email: "hello@gmail.com", password: 321321)
+u3 = User.create(email: "good@gmail.com", password: 321312)
+u4 = User.create(email: "notgood@gmail.com", password: 123423)
