1ac0db52c4c944dfcaa347a8a1a1df0841f3ef94
Sky Lin <linsky519@gmail.com>
2022-01-04 15:44:04 -0700

    handleLogin and handleLogout working

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './providers/AuthProvider';
 
 ReactDOM.render(
+  <AuthProvider>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </AuthProvider>,
   document.getElementById('root')
 );
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+
 const Home = () => {
+  const auth = useContext(AuthContext);
+
   return (
     <div>
       <h1>Home</h1>
+      {JSON.stringify(auth)}
+      <p onClick = {auth.handleLogin}>Login</p>
+      <p onClick = {auth.handleLogout}>Logout</p>
     </div>
   )
 }
